d0cad64c8542f4c4652d42d9638676e5cb9f48f3
coeus <sallahuddinkh@gmail.com>
2021-11-10 11:41:08 +0500

    Removing manual created/update date insertion and implementing success/failure message

--- ecommerceproj/ecommerceproj/settings.py
+++ ecommerceproj/ecommerceproj/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib import messages
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,3 +133,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticFiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+}
--- ecommerceproj/theretailerapp/views.py
+++ ecommerceproj/theretailerapp/views.py
@@ -22,24 +22,22 @@ def insert_customer(request):
         email = request.POST.get("email")
         selected_country  = request.POST.get("country")
         selected_country_obj = Country.objects.get(country_name = selected_country)
-        created_at = request.POST.get("created_at")
-        date_time_obj = datetime.datetime.strptime(created_at, '%Y-%m-%d')
-        created_at_str = date_time_obj.strftime('%Y-%m-%d')
-        updated_at = request.POST.get("updated_at")
-        date_time_obj = datetime.datetime.strptime(updated_at, '%Y-%m-%d')
-        updated_at_str = date_time_obj.strftime('%Y-%m-%d')
-        if(created_at>updated_at):
-            messages.warning(request, 'Updated Date larger than Creation Date')
-        else:
+        created_at = datetime.date.today()
+        created_at_str = created_at.strftime('%Y-%m-%d')
+        try:
             customer_instance = Customer(first_name=first_name,
             last_name=last_name,
             shipping_address=shipping_address,
             billing_address=billing_address,
             ph_number=ph_number,email=email,
             created_at=created_at_str,
-            updated_at=updated_at_str,
+            updated_at=created_at_str,
             )
             customer_instance.counrty = selected_country_obj
             customer_instance.save()
-            HttpResponseRedirect('/')
+        except Exception as e:
+            print(e)
+            messages.error(request,'System was not able to register try later')
+        messages.success(request, 'Customer Accounted Created Succesfully.')
+        return HttpResponseRedirect('/')
     return render(request,'theretailerapp/customer_signup_form.html', {'countries':country},)
