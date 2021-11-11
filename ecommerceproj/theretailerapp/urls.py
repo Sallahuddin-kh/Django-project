977e5e9a13ddd513aa290d7c89596a4e4b94dd36
coeus <sallahuddinkh@gmail.com>
2021-11-11 18:41:51 +0500

    Adding sign-in feature and styling

--- ecommerceproj/theretailerapp/urls.py
+++ ecommerceproj/theretailerapp/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CustomerListView.as_view(), name='customer'),
-    path('product', views.ProductListView.as_view(), name = 'product'),
+    path('', views.product_list_view, name='product'),
+    path('product', views.product_list_view, name = 'product'),
+    path('customer',views.CustomerListView.as_view(),name = 'customer'),
     path('customer/create', views.insert_customer, name='customer_create'),
+    path('customer/login', views.login_customer, name='customer_login'),
+    path('customer/logout', views.customer_sign_out, name='customer_logout'),
 ]
