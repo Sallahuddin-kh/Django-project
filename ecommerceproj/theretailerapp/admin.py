81f99bb1adc49554b98118faf3cceea67c90285c
coeus <sallahuddinkh@gmail.com>
2021-12-03 16:06:49 +0500

    Changing variable names in orderAdmin

--- ecommerceproj/theretailerapp/admin.py
+++ ecommerceproj/theretailerapp/admin.py
@@ -10,9 +10,9 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('customer', 'status' ,'created_at', 'updated_at' , 'order_shipping_address','order_price','order_products')
     list_filter = ('customer__email', 'created_at')
     fields = ('customer', 'status' ,'created_at', 'updated_at' , 'order_shipping_address','order_price')
-    
-    def order_products(self, obj):
-        items = OrderItem.objects.filter(order = obj)
+
+    def order_products(self, order_obj):
+        items = OrderItem.objects.filter(order = order_obj)
         products = []
         for item in items:
             products.append(item.product.product_name)
