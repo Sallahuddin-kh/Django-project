e915c5360a3cad2c5938e0775816696552c3c41f
coeus <sallahuddinkh@gmail.com>
2021-12-09 11:58:10 +0500

    Removing data deletion problem on up

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,11 @@ then
 
     echo "PostgreSQL started"
 fi
-python manage.py flush --no-input
+python manage.py makemigrations
 python manage.py migrate
 DJANGO_SUPERUSER_PASSWORD=123456
 export DJANGO_SUPERUSER_PASSWORD
 python manage.py createsuperuser --no-input --username sallahuddin --email sallahuddin@gmail.com
+python manage.py test
 python manage.py runserver 0.0.0.0:8000
 exec "$@"
